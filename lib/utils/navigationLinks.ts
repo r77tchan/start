29c0006abdd4440bb7d605d3adcb5fc4e0b7edbd
r77tchan <r77tchan@gmail.com>
2026-03-09 19:03:01 +0900

    リンク修正

--- lib/utils/navigationLinks.ts
+++ lib/utils/navigationLinks.ts
@@ -6,6 +6,6 @@ export type NavigationLink = {
 
 export const links: NavigationLink[] = [
   { id: 1, name: "ホーム", href: "/" },
-  { id: 2, name: "メッセージ", href: "/message" },
-  { id: 3, name: "会社概要", href: "/company" },
+  { id: 2, name: "メッセージ", href: "/message/" },
+  { id: 3, name: "会社概要", href: "/company/" },
 ];
