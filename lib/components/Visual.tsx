a3fe1c77ac09f68ed5a27c9125d5b376a211c31d
r77tchan <r77tchan@gmail.com>
2026-03-09 19:34:00 +0900

    メインページh1へ修正

--- lib/components/Visual.tsx
+++ lib/components/Visual.tsx
@@ -13,9 +13,9 @@ export default function Visual() {
       </picture>
 
       <div className="flex h-full flex-col items-center justify-center leading-none text-white">
-        <h2 className="mb-2.5 text-[80px] font-bold tracking-[0px] [text-shadow:0_4px_6px_rgb(0_0_0/25%)] md:text-[160px]">
+        <h1 className="mb-2.5 text-[80px] font-bold tracking-[0px] [text-shadow:0_4px_6px_rgb(0_0_0/25%)] md:text-[160px]">
           START
-        </h2>
+        </h1>
         <p className="text-[20px] font-bold tracking-widest md:text-[24px]">
           仕事に最高のスタートを
         </p>
